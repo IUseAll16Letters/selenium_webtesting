b3c3355759f2d27b85bf007fe4da50752bc53895
IUseAll16Letters <ch1ck3n@mail.ru>
2022-05-24 19:12:09 +0300

    recent updates, test_commit

--- tests/test_items.py
+++ tests/test_items.py
@@ -13,11 +13,13 @@ language_configuration = {'es': 'Añadir al carrito',
 
 def test_locale_languages(browser, request):
     browser.get(source)
-    sleep(3)
+
     language_received = request.config.getoption('language')
     required_response = language_configuration.get(language_received)
-    btn = WebDriverWait(browser, 5).until(ec.element_to_be_clickable((By.CSS_SELECTOR, 'button[value]')))
-    print(btn)
+
+    WebDriverWait(browser, 5).until(ec.element_to_be_clickable((By.CSS_SELECTOR, 'button[value]')))
     res = browser.find_element(by=By.CSS_SELECTOR, value='button[value]').get_attribute('value')
     print(f'\nselected language = {language_received}')
+    sleep(5)
+
     assert required_response == res, f'Expected get {required_response}, got {res} instead'
